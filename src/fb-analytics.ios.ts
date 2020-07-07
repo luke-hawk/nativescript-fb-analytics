7d3c69d0bbb5bef53fccae5c5791c1b4f312097c
luke-hawk <25054@novasbe.pt>
2020-07-07 11:41:11 +0200

    Initial commit

---
+++ src/fb-analytics.ios.ts
@@ -0,0 +1,5 @@
+import { Common } from './fb-analytics.common';
+
+export class FbAnalytics extends Common {
+
+}
---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import { Common } from './fb-analytics.common';
+export declare class FbAnalytics extends Common {
+  // define your typings manually
+  // or..
+  // take the ios or android .d.ts files and copy/paste them here
+}
